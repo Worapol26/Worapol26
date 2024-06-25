e73be4db01110e0f229056c15a490d1cec5f6d8f
Worapol Uttasara <kaiouzaa1234@gmail.com>
2024-06-25 14:43:53 +0700

    frist

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,46 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  //ตัวแปรรับค่า input
+  weight ? : number;
+  hight ? : number;
+
+  //ตัวแสดงผลลัพธ์
+  result ? : number;
+  result_text ? : string ="";
+
   constructor() {}
 
+  oncal(){
+    console.log("w : " + this.weight)
+    console.log("h : " + this.hight)
+if(this.weight == undefined || this.hight == undefined) {
+   alert("กรุณากรอกข้อมูลให้ครบท่วน");
+   return;
+  }
+  var hi = this.hight / 100;
+  this.result = this.weight / (hi * hi);
+    console.log("ผลลัพธ์ : " + this.result)
+
+  if(this.result < 18.5){
+   // console.log("ผอมเกินไป")
+   this.result_text = "ผอมเกินไป";
+    }else if(this.result >=18.6 && this.result <=24) {
+    //  console.log("น้ำหนักปกติ เหมาะสม")
+    this.result_text = "น้ำหนักปกติ เหมาะสม";
+    }else if(this.result >=25 && this.result <=29.9) {
+   //   console.log("อ้วน");
+   this.result_text = "อ้วน";
+
+    }else{
+   //   console.log("อ้วนมาก");
+   this.result_text = "อ้วนมาก";
+    }
+  
+
+
+
+  }
 }
+
+
